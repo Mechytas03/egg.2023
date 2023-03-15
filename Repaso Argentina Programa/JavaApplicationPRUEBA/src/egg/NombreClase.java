3461697379ab0ef0ea902a6551fee34617bcab4a
mechytas03 <mechys2007@gmail.com>
2023-03-15 09:56:03 -0300

    codeando+

---
+++ Repaso Argentina Programa/JavaApplicationPRUEBA/src/egg/NombreClase.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//cuando creamos la clase psvn para ejecutar el programa 
+package egg;
+
+/**
+ *psvm mas tab
+ * @author Luz Fernandez
+ */
+import java.util.Scanner;
+public class NombreClase1 {
+    public static void main(String[] args) {
+       //aca se ejectuta el programa
+       //int num declarar una variable como entero
+       //num = o inicializar
+       //num=1; asignacion
+       //declariacion e inicializar en la misma sentencia
+       //++ incremento -- incremento 
+       //++ numero sumar y mostras , numero ++ mostrar numero y sumar
+       //== comparacion = asignacion 
+       //concatenar una var + (para operaciones matematicas)
+       // las letras se gurdan con''
+       //concat concatenar
+       //System.out.println para imprimir
+       //contains v o f si tiene el caracter subcadena
+       //para leer
+       Scanner leer = new Scanner (System.in);
+       System.out.println("ingrese dos numeros");
+       int num1 = leer.nextInt();
+       int num2 = leer.nextInt();
+       
+       int suma = num1 + num2;
+       System.out.println("el resultado es" + suma);
+       
+               
+               
+               
+       
+    }
+}
+public class NombreClase2 {
+    public static void main(String[] args) {
+     Scanner leer = new Scanner(System.in);
+     System.out.println("ingrese su nombre");
+     String nombre = leer.next();
+     System.out.println(nombre);
+     
+       
+               
+               
+               
+       
+    }
+}
+public class NombreClase3 {
+    public static void main(String[] args) {
+     Scanner leer = new Scanner(System.in);
+     System.out.println("ingrese una frase pedorra");
+     String frase = leer.nextLine();
+     System.out.println(frase.toLowerCase());
+     System.out.println(frase.toUpperCase());
+     
+       
+               
+               
+               
+       
+    }
+}
